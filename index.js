b2d8956e861227452dbf3ceb636176694606e036
antonivegaochoa <tonivegaochoa@gmail.com>
2021-03-04 17:29:16 -0500

    added 1x3 grid functionality

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const board = document.querySelector('#board');
 const spaces = board.querySelectorAll('.space');
 const spacesArr = Array.from(spaces);
-const tile = board.querySelector('.tile');
+const tiles = board.querySelectorAll('.tile');
 
-tile.addEventListener('click', moveToAdjacent);
+tiles.forEach(tile => tile.addEventListener('click', moveToAdjacent));
 
 function moveToAdjacent() {
-  const index = spacesArr.findIndex(element => element.firstChild === this);
+  const index = spacesArr.findIndex(e => e.firstChild === this);
+  const emptyIndex = spacesArr.findIndex(e => e.childElementCount === 0);
+  const diff = Math.abs(emptyIndex - index);
+  console.log(`index = ${index}, emptyIndex = ${emptyIndex}, diff = ${diff}`);
 
-  if(spacesArr[index+1] && spacesArr[index+1].childElementCount === 0) {
-    spacesArr[index+1].appendChild(this);
-  } else {
-    spacesArr[index-1].appendChild(this);
-  };
+  if(diff === 1) {
+    if(index < emptyIndex) {
+      spacesArr[index+1].appendChild(this);
+    } else {
+      spacesArr[index-1].appendChild(this);
+    }
+  } else if(diff > 1) {
+    if(index < emptyIndex) {
+      for(let i = index; i < emptyIndex; i++) {
+        spacesArr[i+1].appendChild(spacesArr[i].firstChild);
+      }
+    } else {
+      for(let i = emptyIndex; i < index; i++) {
+        spacesArr[i].appendChild(spacesArr[i+1].firstChild);
+      }
+    }
+  }
 }
+
+const gameBoard = (() => {
+  const board = [];
+})();
