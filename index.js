697115dfb0a4fa283e1f99937cdf65e8ef1974c5
antonivegaochoa <tonivegaochoa@gmail.com>
2021-03-04 21:47:50 -0500

    functional 3x3 game

--- index.js
+++ index.js
@@ -1,47 +1,109 @@
-const board = document.querySelector('#board');
-const spaces = board.querySelectorAll('.space');
-const spacesArr = Array.from(spaces);
-const tiles = board.querySelectorAll('.tile');
-
-tiles.forEach(tile => tile.addEventListener('click', moveToAdjacent));
-
-function moveToAdjacent() {
-  const index = spacesArr.findIndex(e => e.firstChild === this);
-  const emptyIndex = spacesArr.findIndex(e => e.childElementCount === 0);
-  const diff = Math.abs(emptyIndex - index);
-  console.log(`index = ${index}, emptyIndex = ${emptyIndex}, diff = ${diff}`);
-
-  if(diff === 1) {
-    if(index < emptyIndex) {
-      spacesArr[index+1].appendChild(this);
-    } else {
-      spacesArr[index-1].appendChild(this);
-    }
-  } else if(diff === 3) {
-    spacesArr[emptyIndex].appendChild(this);
-  } else if(diff === 2) {
-    if(index < emptyIndex) {
-      for(let i = index; i < emptyIndex; i++) {
-        spacesArr[i+1].appendChild(spacesArr[i].firstChild);
-      }
-    } else {
-      for(let i = emptyIndex; i < index; i++) {
-        spacesArr[i].appendChild(spacesArr[i+1].firstChild);
-      }
-    }
-  } else if(diff === 6) {
-    if(index < emptyIndex) {
-      for(let i = index; i < emptyIndex; i+=3) {
-        spacesArr[i+3].appendChild(spacesArr[i].firstChild);
-      }
-    } else {
-      for(let i = emptyIndex; i < index; i+=3) {
-        spacesArr[i].appendChild(spacesArr[i+3].firstChild);
-      }
-    }
-  }
-}
+"use strict";
 
 const gameBoard = (() => {
-  const board = [];
+  const board = Array.from(document.querySelectorAll('.space'));
+  const tiles = Array.from(document.querySelectorAll('.tile'));
+  const size = 3;
+  const numSpaces = size*size;
+
+  tiles.forEach(tile => tile.addEventListener('click', tileClicked));
+
+  function tileClicked() {
+    const index = board.findIndex(e => e.firstChild === this);
+    const emptyIndex = board.findIndex(e => e.childElementCount === 0);
+    const diff = Math.abs(emptyIndex - index);
+    const row = Math.floor(index / size);
+    const col = index % size;
+
+    let inRow = false;
+    let inCol = false;
+
+    // check for empty space in row
+    for(let i = row*size; i < row*size+size; i++) {
+      if(i === emptyIndex) {
+        inRow = true;
+        break;
+      };
+    }
+
+    // check for empty space in column
+    for(let i = col; i < board.length; i+=size) {
+      if(i === emptyIndex) {
+        inCol = true;
+        break;
+      }
+    }
+
+    if(inRow) {
+      if(index < emptyIndex) {
+        for(let i = index; i < emptyIndex; i++) {
+          board[i+1].appendChild(board[i].firstChild);
+        }
+      } else {
+        for(let i = emptyIndex; i < index; i++) {
+          board[i].appendChild(board[i+1].firstChild);
+        }
+      }
+    } else if (inCol) {
+      if(index < emptyIndex) {
+        for(let i = index; i < emptyIndex; i+=size) {
+          board[i+size].appendChild(board[i].firstChild);
+        }
+      } else {
+        for(let i = emptyIndex; i < index; i+=size) {
+          board[i].appendChild(board[i+size].firstChild);
+        }
+      }
+    }
+
+    if(solved()) {
+      setTimeout(displayController.solved, 100);
+    }
+  };
+
+  const solved = () => {
+    if(board[numSpaces-1].firstChild) return false;
+    return board.every((space,index) => {
+      if(!space.firstChild) return true;
+      return space.firstChild.id == index+1;
+    });
+  }
+
+  return {board, tiles, numSpaces};
+})();
+
+const gameController = (() => {
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  const shuffle = () => {
+    shuffleArray(gameBoard.tiles);
+    gameBoard.board.forEach((space, index) => {
+      if(index < gameBoard.numSpaces-1) {
+        space.appendChild(gameBoard.tiles[index]);
+      }
+    })
+  };
+
+  const newGame = () => {
+    shuffle();
+  };
+
+  return {newGame};
+})();
+
+const displayController = (() => {
+  const newGameBtn = document.querySelector('button');
+
+  newGameBtn.addEventListener('click', gameController.newGame);
+
+  const solved = () => {
+    window.alert('You solved the puzzle');
+  }
+
+  return {solved};
 })();
