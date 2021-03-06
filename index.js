b7b6ccb44246f8f0102fbd10655a522072657084
antonivegaochoa <tonivegaochoa@gmail.com>
2021-03-05 20:02:56 -0500

    added game menu w/ dynamic puzzle size creation and deletion

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 "use strict";
 
-const gameBoard = (() => {
+const gameBoard = (size) => {
   const board = Array.from(document.querySelectorAll('.space'));
   const tiles = Array.from(document.querySelectorAll('.tile'));
-  const size = 3;
-  const numSpaces = size*size;
 
   const event = new Event('click');
-
   tiles.forEach(tile => tile.addEventListener('click', tileClicked));
 
-  function tileClicked(e) {
+  function tileClicked() {
     const index = board.findIndex(e => e.firstChild === this);
     const emptyIndex = board.findIndex(e => e.childElementCount === 0);
-    const diff = Math.abs(emptyIndex - index);
     const row = Math.floor(index / size);
     const col = index % size;
 
@@ -59,22 +55,88 @@ const gameBoard = (() => {
     }
   };
 
-  return {board, tiles, size, numSpaces, event};
-})();
+  const getSize = () => size;
+
+  return {board, tiles, event, getSize};
+};
 
 const gameController = (() => {
+  let game;
+
   const shuffle = () => {
-    for(let i = 0; i < 100*gameBoard.size; i++) {
-      const r = Math.floor(Math.random() * 100 % 8);
-      gameBoard.tiles[r].dispatchEvent(gameBoard.event);
+    for(let i = 0; i < 100*game.getSize(); i++) {
+      const r = Math.floor(Math.random() * 100 % game.tiles.length);
+      game.tiles[r].dispatchEvent(game.event);
     }
   };
 
-  return {shuffle};
+  const startGame = (size) => {
+    game = gameBoard(size);
+  };
+
+  return {shuffle, startGame};
 })();
 
 const displayController = (() => {
-  const shuffleBtn = document.querySelector('button');
+  const container = document.querySelector('#container');
+  const gameContainer = container.querySelector('#game');
+  const menuContainer = container.querySelector('#menu');
+  const menuBtn = gameContainer.querySelector('#menuBtn');
+  const shuffleBtn = gameContainer.querySelector('#shuffleBtn');
+  const board = gameContainer.querySelector('#board');
+  const boardSizeBtns = menuContainer.querySelectorAll('.boardSize');
 
+  menuBtn.addEventListener('click', showPuzzleOptions);
   shuffleBtn.addEventListener('click', gameController.shuffle);
+  boardSizeBtns.forEach(btn => btn.addEventListener('click', startGame));
+
+  function showPuzzleOptions() {
+    gameContainer.classList.toggle('hidden');
+    menuContainer.classList.toggle('hidden');
+    clearBoard();
+  };
+
+  function startGame() {
+    gameContainer.classList.toggle('hidden');
+    menuContainer.classList.toggle('hidden');
+
+    const size = this.getAttribute('size');
+    createBoard(size);
+    gameController.startGame(Number(size));
+  };
+
+  function clearBoard() {
+    while(board.firstChild) {
+      board.removeChild(board.firstChild);
+    }
+  }
+
+  function createBoard(size) {
+    let columns = "";
+    for(let i = 0; i < size; i++) columns += 'auto ';
+    board.style.gridTemplateColumns = columns;
+
+    for(let i = 1; i < size*size; i++) {
+      const space = createSpace();
+      const tile = createTile(i);
+      space.appendChild(tile);
+      board.appendChild(space);
+    }
+
+    board.appendChild(createSpace());
+  };
+
+  function createSpace() {
+    const div = document.createElement('div');
+    div.classList.add('space');
+    return div;
+  };
+
+  function createTile(num) {
+    const div = document.createElement('div');
+    div.classList.add('tile');
+    div.setAttribute('id', num);
+    div.textContent = num;
+    return div;
+  };
 })();
